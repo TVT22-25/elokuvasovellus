9fd5a84d3861269116b44c4ebc94d7bfed146c88
Jykakr <112549827+Jykakr@users.noreply.github.com>
2023-12-14 22:29:33 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'swiper/css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 //import Main from './Components/Main.js';
 //import Movie from './Movie';
 //import './Components/style.css';
 //import Card from './Components/Card.js';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './pages/Header.jsx';
 import Banner from './pages/Banner.jsx';
-import SearchResults from './Components/SearchResults.jsx';
 import ReviewPage from './pages/Review';
+import SearchResults from './Components/SearchResults.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import Reviews from './pages/Reviews';
 
 function App() {
 
@@ -33,6 +36,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Banner />} />
           <Route path="/search-results" element={<SearchResults />} />
+          <Route path="/review" element={<ReviewPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/:id/reviews" element={<Reviews />} />
         </Routes>
       </>
     </Router>
