a75906b66d2443fc537bd0e45cfd87fcff1e79bf
Terhi Repo <t0rete00@students.oamk.fi>
2023-12-15 15:56:49 +0200

    Add GET_ALL_USER_SETTINGS

--- server/database/settings.js
+++ server/database/settings.js
@@ -1,6 +1,7 @@
 const { pgPool } = require('./connection.js');
 
 const sql = {
+  GET_ALL_USER_SETTINGS: 'SELECT * FROM user_settings',
   GET_USER_SETTINGS: 'SELECT * FROM user_settings WHERE (user_id = $1)',
   CREATE_USER_SETTINGS: 'INSERT INTO user_settings (user_id, show_movies, show_news, show_reviews, show_posts) VALUES ($1, $2, $3, $4, $5)',
   UPDATE_USER_SETTINGS: 'UPDATE user_settings SET {columns} WHERE user_id=$1',
@@ -14,6 +15,9 @@ const getUserSettings = async(user_id) => {
       ? { code: 202, content: result.rows }
       : { code: 404, content: { error: 'Settings not found with user_id' } };
     }
+
+    const result = await pgPool.query(sql.GET_ALL_USER_SETTINGS);
+    return { code: 202, content: result.rows };  
 };
 
 const createUserSettings = async (user_id) => {
